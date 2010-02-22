d18d190caefb5d4e0c80b24dc29dc56c00d71938
ralf <ralfbaumbach@gmail.com>
2010-02-22 15:33:43 +0100

    changed style

--- templates/sticky-notes-info-box.tpl.php
+++ templates/sticky-notes-info-box.tpl.php
@@ -4,16 +4,14 @@
       
       <?php if ($add_link): ?>
         <div id="sticky-notes-add-note-button">
-          <a href="<?php print $add_link; ?>">+ Add Note</a>
+          <a href="<?php print $add_link; ?>"><?php print t('+ Add note'); ?></a>
         </div>
-        <br />
       <?php endif; ?>
       
       <div id="sticky-notes-page-count"></div>
-      <br />
       
       <div id="sticky-notes-options">
-        <a id="sticky-notes-options-show-all" href="#">show all</a>
+        <a id="sticky-notes-options-show-all" href="#"><?php print t('show all'); ?></a>
       </div>
       
     </div>
